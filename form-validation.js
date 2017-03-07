1239a88b88707f3ad85d7a4ac5bb8bc068bbb2b9
Unknown <mshimada2@mail.csuchico.edu>
2017-03-07 13:02:34 -0800

    added conditional statements to make it show errr messages

--- form-validation.js
+++ form-validation.js
@@ -3,17 +3,41 @@ function validate(){
   var userEntered = document.getElementById("user").value;
   var passEntered = document.getElementById("pass").value;
 
-  //Show message that there is an error with the username...
-  document.getElementById("usernameError").innerHTML="Bad username.";
-  document.getElementById("usernameError").classList.remove("hidden-message");
-  document.getElementById("usernameError").classList.add("shown-message");
+  if (userEntered.length < 6){
+    document.getElementById("usernameError").innerHTML="Bad username.";
+    document.getElementById("usernameError").classList.remove("hidden-message");
+    document.getElementById("usernameError").classList.add("shown-message");
+    //Turn the username items red
+    document.getElementById("usernameGroup").classList.add("has-error");}
+else{
+  document.getElementById("usernameError").innerHTML="Good username.";
+  document.getElementById("usernameError").classList.add("hidden-message");
+  document.getElementById("usernameError").classList.remove("shown-message");
   //Turn the username items red
-  document.getElementById("usernameGroup").classList.add("has-error");
+  document.getElementById("usernameGroup").classList.add("has-success");
+}
 
-  //Show message that there is an error with the password...
+
+
+
+
+if (passEntered == "password"){
   document.getElementById("passwordError").innerHTML="Bad password.";
   document.getElementById("passwordError").classList.remove("hidden-message");
   document.getElementById("passwordError").classList.add("shown-message");
   //Turn the password items red
   document.getElementById("passwordGroup").classList.add("has-error");
+}
+else{
+  document.getElementById("usernameError").innerHTML="Good password.";
+  document.getElementById("usernameError").classList.add("hidden-message");
+  document.getElementById("usernameError").classList.remove("shown-message");
+  //Turn the username items red
+  document.getElementById("usernameGroup").classList.add("has-success");
+}
+
+
+
+
+
 }
